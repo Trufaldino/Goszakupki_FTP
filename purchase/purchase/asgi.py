bf96c8cc50638c15d550b2f6aaeabf5f17457426
anton.ryadinski <anton.ryadinski@gmail.com>
2023-07-07 11:26:02 +0300

    renamed files and project, addition to gitignore

--- purchase/purchase/asgi.py
+++ purchase/purchase/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ftp_purchase.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'purchase.settings')
 
 application = get_asgi_application()
--- purchase/purchase/wsgi.py
+++ purchase/purchase/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ftp_purchase.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'purchase.settings')
 
 application = get_wsgi_application()
