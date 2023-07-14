dcbcd3746510b82419f039b1e57d83d9dd861c34
Trufaldino <anton.ryadisnki@gmail.com>
2023-07-14 17:09:02 +0300

    popover added

--- purchase/purchase/settings.py
+++ purchase/purchase/settings.py
@@ -79,13 +79,6 @@ WSGI_APPLICATION = 'purchase.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'database.db',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql'),
