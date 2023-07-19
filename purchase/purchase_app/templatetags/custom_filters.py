6cc6f0637cf61b4c130cc1172b8b9a3b2952e01d
Trufaldino <anton.ryadisnki@gmail.com>
2023-07-19 14:16:33 +0300

    Formating dates and prices changed

--- purchase/purchase_app/templatetags/custom_filters.py
+++ purchase/purchase_app/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from babel.numbers import format_currency
 
 
 register = template.Library()
@@ -7,3 +8,13 @@ register = template.Library()
 @register.filter
 def get_field_value(request, field_name):
     return getattr(request, field_name, None)
+
+
+@register.filter
+def space_separated(value):
+    try:
+        price = float(value)
+        formatted_price = format_currency(price, 'RUB', locale='ru_RU', format='#,##0.00')
+        return formatted_price
+    except (ValueError, TypeError):
+        return value
